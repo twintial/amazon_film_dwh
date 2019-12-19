b19fa83ce0db26a27b6a2952e6494c2c8e018c76
hailuo <550847434@qq.com>
2019-12-19 15:06:11 +0800

    第一次提交

---
+++ src/main/java/com/example/dwh/DwhApplication.java
@@ -0,0 +1,13 @@
+package com.example.dwh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwhApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/dwh/controller/SearchController.java
@@ -0,0 +1,12 @@
+package com.example.dwh.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class SearchController {
+}
